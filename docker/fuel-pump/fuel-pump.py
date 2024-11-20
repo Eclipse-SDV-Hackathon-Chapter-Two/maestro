7c7fd7d4c4ca3c59e58c834b141faf0d1cb16502
Patrick Draxler <patrick1471@hotmail.com>
2024-11-20 20:13:16 +0100

    update fuel-pump.py to incrementally update the fuel in a thread

--- docker/fuel-pump/fuel-pump.py
+++ docker/fuel-pump/fuel-pump.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 import json
 import os
 import time
+import threading
 
 app = FastAPI()
 
@@ -22,7 +23,19 @@ def save_fuel_state(state):
     with open(FUEL_STATE_FILE, 'w') as file:
         json.dump(state, file)
 
+fuel_lock = threading.Lock()
+fuel_thread = None
 fuel_state = load_fuel_state()
+
+def fill_fuel(update: FuelUpdate):
+    global fuel_state
+    while update.current_fuel < update.target:
+        with fuel_lock:
+            update.current_fuel += 1
+            fuel_state['fuel_percent'] = update.current_fuel
+            save_fuel_state(fuel_state)
+        time.sleep(1)        
+
 
 @app.get("/fuel")
 def get_fuel_state():
@@ -32,8 +45,10 @@ def get_fuel_state():
 def update_fuel_state(update: FuelUpdate):
     if update.current_fuel < 0 or update.target <= 0:
         raise HTTPException(status_code=400, detail="Invalid fuel values")#
-    while update.current_fuel < update.target:
-        update.current_fuel += 1
-        save_fuel_state(fuel_state)
-        time.sleep(1)
-    return fuel_state
+    global fuel_thread
+    if fuel_thread is None or not fuel_thread.is_alive():
+        fuel_thread = threading.Thread(target=fill_fuel, args=(update,))
+        fuel_thread.start()
+    else:
+        raise HTTPException(status_code=400, detail="Fuel update already in progress")
+    return 200
